a2e4ca67ae53ce730d092d36b53f980e96fee4e5
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-08-06 23:38:45 +0300

    Фикс начала избавления от дублирования

--- app/src/main/java/pro/progr/todos/brightcards/composable/BrightCard.kt
+++ app/src/main/java/pro/progr/todos/brightcards/composable/BrightCard.kt
@@ -9,16 +9,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import pro.progr.todos.DiamondViewModel
+import pro.progr.todos.brightcards.CardStyle
 import pro.progr.todos.brightcards.composable.description.Description
 import pro.progr.todos.brightcards.composable.title.Title
 import pro.progr.todos.brightcards.vm.ListedCardViewModel
 
 @Composable
 fun BrightCard(viewModel: ListedCardViewModel, diamondViewModel: DiamondViewModel, onclick: () -> Unit = {}) {
+    val style = remember { CardStyle(viewModel.card) }
     Column(
         modifier = Modifier
             .padding(horizontal = 15.dp, vertical = 5.dp)
@@ -37,7 +40,9 @@ fun BrightCard(viewModel: ListedCardViewModel, diamondViewModel: DiamondViewMode
     ) {
 
 
-        Title(viewModel = viewModel, diamondViewModel = diamondViewModel)
+        Title(viewModel = viewModel,
+            diamondViewModel = diamondViewModel,
+            style = style)
 
         Description(viewModel = viewModel)
 
