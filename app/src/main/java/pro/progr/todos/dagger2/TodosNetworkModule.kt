dbc0a1edda2efdcf697a7ecb1f4d71e6febaab61
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-08-24 22:49:30 +0300

    TodosNetworkModule копипаст

---
+++ app/src/main/java/pro/progr/todos/dagger2/TodosNetworkModule.kt
@@ -0,0 +1,53 @@
+package pro.progr.todos.dagger2
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+object TodosNetworkModule {
+
+    @Provides @Singleton
+    fun provideGson(): Gson = GsonBuilder()
+        // .setDateFormat("yyyy-MM-dd'T'HH:mm:ssX") // если нужно
+        .create()
+
+    @Provides @Singleton
+    fun provideConverterFactory(gson: Gson): Converter.Factory =
+        GsonConverterFactory.create(gson)
+
+    @Provides @Singleton
+    fun provideOkHttp(
+        // подставь интерцепторы при необходимости
+    ): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides @Singleton
+    fun provideRetrofit(
+        baseUrl: String,
+        okHttp: OkHttpClient,
+        converterFactory: Converter.Factory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl) // ДОЛЖЕН заканчиваться на '/'
+        .client(okHttp)
+        .addConverterFactory(converterFactory)
+        .build()
+
+    @Provides @Singleton
+    fun provideTodosApi(retrofit: Retrofit): TodosApiService =
+        retrofit.create(TodosApiService::class.java)
+}
