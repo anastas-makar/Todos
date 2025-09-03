1fbd1e0cdaeac0cd461899628bf4545d14fba944
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-09-03 18:49:50 +0300

    колбэк в процессе (триггеры и таблицы для синка)

--- app/src/main/java/pro/progr/todos/db/TodosDataBase.kt
+++ app/src/main/java/pro/progr/todos/db/TodosDataBase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import pro.progr.diamondsandberries.db.FilterTypeConverter
 import pro.progr.diamondsandberries.db.PatternDatesConverter
 import pro.progr.diamondsandberries.db.ScheduleDatesConverter
@@ -49,6 +50,36 @@ abstract class TodosDataBase : RoomDatabase() {
                     "todos_database"
                 )
                 .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_1_3, MIGRATION_1_4, MIGRATION_2_4, MIGRATION_3_4)
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onOpen(db: SupportSQLiteDatabase) {
+                            db.execSQL("""
+                CREATE TABLE IF NOT EXISTS outbox(
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  table_name TEXT NOT NULL,
+                  row_id TEXT NOT NULL,
+                  op TEXT NOT NULL,
+                  created_at INTEGER NOT NULL
+                );
+            """.trimIndent())
+
+                            // Набор триггеров для всех нужных таблиц:
+                            db.execSQL("""
+                CREATE TRIGGER IF NOT EXISTS trg_notes_ins_outbox
+                AFTER INSERT ON notes BEGIN
+                  INSERT INTO outbox(table_name,row_id,op,created_at)
+                  VALUES('notes', NEW.id, 'UPSERT', CAST(strftime('%s','now') AS INTEGER)*1000);
+                END;
+            """.trimIndent())
+                            db.execSQL("""
+                CREATE TRIGGER IF NOT EXISTS trg_notes_upd_outbox
+                AFTER UPDATE ON notes BEGIN
+                  INSERT INTO outbox(table_name,row_id,op,created_at)
+                  VALUES('notes', NEW.id, 'UPSERT', CAST(strftime('%s','now') AS INTEGER)*1000);
+                END;
+            """.trimIndent())
+                            // todo: для tags, note_to_tag_xref, и т.д.
+                        }
+                    })
                 .build()
                 INSTANCE = instance
                 instance
